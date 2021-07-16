1724f25187a0f4f8b16160b755821208c773fca4
Kian Abbott <kianabbott@college.harvard.edu>
2021-07-16 12:38:08 -0400

    edit sidebar scrolling

--- public/js/script.js
+++ public/js/script.js
@@ -2,13 +2,22 @@
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
+  var top;
 
   document.addEventListener('click', function(e) {
     var target = e.target;
 
     if(!checkbox.checked ||
        sidebar.contains(target) ||
-       (target === checkbox || target === toggle)) return;
+       (target === checkbox || target === toggle)){
+         if(target === toggle){
+           top = document.scrollingElement.scrollTop;
+         }
+         if(top !== undefined && (target === checkbox || target === toggle)){
+           document.scrollingElement.scrollTop = top;
+         }
+         return;
+       }
 
     checkbox.checked = false;
   }, false);
